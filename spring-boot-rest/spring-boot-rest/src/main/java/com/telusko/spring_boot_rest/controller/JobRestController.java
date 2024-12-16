f8f4590b5c2962e32cfe2539825366233bd32dcd
Charan7221 <116345427+Charan7221@users.noreply.github.com>
2024-12-16 18:06:06 +0530

    Add files via upload

---
+++ spring-boot-rest/spring-boot-rest/src/main/java/com/telusko/spring_boot_rest/controller/JobRestController.java
@@ -0,0 +1,48 @@
+package com.telusko.spring_boot_rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.telusko.spring_boot_rest.model.JobPost;
+import com.telusko.spring_boot_rest.service.JobService;
+
+@RestController
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+
+    @GetMapping("jobPost")
+    public List<JobPost> getAllJobs() {
+        return service.getAllJobs();
+
+    }
+
+    @GetMapping("/jobPost/{postId}")
+    public JobPost getJob(@PathVariable int postId) {
+        return service.getJob(postId);
+    }
+
+    @PostMapping("jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost) {
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost) {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable int postId)
+    {
+        service.deleteJob(postId);
+        return "Deleted";
+    }
+
+
+}
